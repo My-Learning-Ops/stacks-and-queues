d82781fc1eaae4482fd32af80951b2907cf986e2
Brendan Dileo <dileob23@gmail.com>
2025-06-09 11:51:55 -0400

    Implement pop, peek, and to string methods

--- src/Stack.java
+++ src/Stack.java
@@ -38,12 +38,37 @@ public class Stack<T> {
         top = new Node<T>(item, top);
     }
 
+    // Pops the top node from the stack, returns its value
+    public T pop() throws IllegalStateException {
+        // If the stack isnt empty then we can pop the top node
+        if (!isEmpty()) {
+            T temp = top.value;
+            top = top.next;
+            return temp;
+        }
+        throw new IllegalStateException("Stack Underflow: No items can be popped from the stack, its empty!");
+    }
 
+    // Peeks at the value of the node at the top of the stack
+    public T peek() throws IllegalStateException {
 
+        if (!isEmpty()) {
+            return top.value;
+        }
+        throw new IllegalStateException("Stack Underflow: No items can be peeked from the stack, its empty!");
+    }
 
-
-
-
+    // Returns a string representation of the stack
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        Node<T> current = top;
+        while (current != null) {
+            sb.append(current.value).append(" ");
+            current = current.next;
+        }
+        return sb.toString().trim();
+    }
 
 
 }
