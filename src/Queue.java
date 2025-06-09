a6d61d2ea0f616c8cf29164e354b0043f28379e9
Brendan Dileo <dileob23@gmail.com>
2025-06-09 11:54:56 -0400

    Implement node class for queue

--- src/Queue.java
+++ src/Queue.java
@@ -1,2 +1,33 @@
-public class Queue {
+
+
+
+
+
+
+// A generic implementation of a queue data structure backed by a linked list
+public class Queue<T> {
+
+    // Represents a node in the queue
+    class Node<T> {
+        T value;
+        Node<T> next;
+
+        // Constructs a node with a value and link
+        Node(T val, Node<T> n) {
+            value = val;
+            next = n;
+        }
+    }
+
+    
+
+
+
+
+
+
+
+
+
+
 }
