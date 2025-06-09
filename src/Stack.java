4fb21562250df8d810a675cc63f21afb9ab9339e
Brendan Dileo <dileob23@gmail.com>
2025-06-09 11:45:53 -0400

    Setup stack node class

--- src/Stack.java
+++ src/Stack.java
@@ -1,6 +1,39 @@
 
 
 
+// A generic implementation of a stack data structure backed by a linked list
+public class Stack<T> {
 
-public class Stack {
+    // Represents a node in the stack
+    class Node<T> {
+        T value;
+        Node<T> next;
+        
+        // Constructs a node with a value and link to the next node
+        Node(T val, Node<T> n) {
+            value = val;
+            next = n;
+        }
+
+        // Constructs a node with a value and no link
+        Node(T val) {
+            this(val, null);
+        }
+    }
+
+    private Node<T> top;
+
+    // Constructs an empty stack
+    public Stack() {
+        top = null;
+    }
+
+    
+
+
+
+
+
+
+
 }
