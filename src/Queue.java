97e0ce16d1fe1ee655effa6e401d9d984070bc66
Brendan Dileo <dileob23@gmail.com>
2025-06-09 11:57:18 -0400

    Implement length, peek, and is empty methods

--- src/Queue.java
+++ src/Queue.java
@@ -19,7 +19,24 @@ public class Queue<T> {
         }
     }
 
-    
+
+    private Node<T> first = null;
+    private Node<T> last = null;
+    private int count = 0;
+
+    // Returns the length of the queue
+    public int length() { return count; }
+
+    // Peeks at the value of the first node in the queue
+    public T peek() { return first.value; }
+
+    // Checks if the queue is empty
+    public boolean isEmpty() { return count == 0; }
+
+    public void enqueue(T item) {
+        
+    }
+
 
 
 
